e5e73fcc047819e47a7d0e5ee3b5f714fcbf7edb
Russ Frisch <russ.frisch@gmail.com>
2020-03-25 10:22:08 -0400

    Initial commit

---
+++ src/main/java/com/opsera/code/deployer/CodeDeployerApplication.java
@@ -0,0 +1,13 @@
+package com.opsera.code.deployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDeployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDeployerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opsera/code/deployer/CodeDeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opsera.code.deployer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDeployerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
